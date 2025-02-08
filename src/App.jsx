185d92744dff2c8219d01926de3af0ca5895c6ec
ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2025-02-08 14:41:44 -0500

    meter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Domain from './components/domain';
 import './App.css';
+import Score from './components/score';
 
 function App() {
 	return (
 		<div className="container flex justify-center items-center">
 			<img src="/name.svg" alt="name" className="name" />
 			<Domain domain="facebook.com" lastChecked="2 hours ago" />
+			<Score score={75} />
 		</div>
 	);
 }
